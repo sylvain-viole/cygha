725084ac2fa28865dde08b31d425ca6fab6b319c
Sylvain Viole <sylvainviole@gmail.com>
2023-03-02 08:35:16 +0100

    add writing permissions

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,6 +1,6 @@
 describe("this is a test", () => {
   it("should see Bienvenue !", () => {
-    cy.visit("localhost:3000/");
+    cy.visit("https://sylvain-viole.github.io/cygha/src");
     cy.contains("Bienvenue !").should("be.visible");
   });
 });
