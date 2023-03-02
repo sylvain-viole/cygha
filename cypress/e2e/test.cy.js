b2b215e210f77f92c47cc041d9160bd1c7a34856
sylvain-viole <71819292+sylvain-viole@users.noreply.github.com>
2023-03-02 07:44:33 +0000

    Deploying to gh-pages from @ sylvain-viole/cygha@2026eabb9f2224d334a46eac0562d67030dd6b40 🚀

--- cypress/e2e/test.cy.js
+++
@@ -1,6 +0,0 @@
-describe("this is a test", () => {
-  it("should see Bienvenue !", () => {
-    cy.visit("https://sylvain-viole.github.io/cygha/");
-    cy.contains("Bienvenue !").should("be.visible");
-  });
-});
