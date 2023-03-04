7e828444cd99c18b5a55121d84a15294d5df0519
Sylvavio <71819292+sylvain-viole@users.noreply.github.com>
2023-03-04 10:47:43 +0100

    add mochawesome

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,15 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "cypress/results",
+    reportFilename: "[name].html",
+    overwrite: true,
+    html: true,
+    json: false,
+  },
+  video: false,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
