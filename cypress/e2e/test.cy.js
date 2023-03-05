42f3772684a039937cc6c19a25078b20a6e512bb
Sylvain Viole <sylvainviole@gmail.com>
2023-03-05 07:51:32 +0100

    improve tests and npm commands

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,15 +1,31 @@
 const { beforeEach } = require("mocha");
 
-describe("testing our test page", () => {
+describe("On this testing page I should...", () => {
   beforeEach(() => {
     cy.visit("/");
   });
 
-  it("I should see Bienvenue !", () => {
-    cy.contains("Bienvenue !").should("be.visible");
+  it('...See "Bienvenue !" as a title', () => {
+    cy.contains("h1", "Bienvenue !").should("be.visible");
   });
 
-  it("I should click the button !", () => {
-    cy.get("button#main_button").click();
+  it('...See "Automation Tester Academy" as a valid link opening a new tab', () => {
+    cy.contains("a", "Automation Tester Academy")
+      .should("be.visible")
+      .then((link) => {
+        cy.wrap(link)
+          .should("have.attr", "target", "_blank")
+          .should("have.attr", "href")
+          .and("contain", "https://formations.automationtesteracademy.com/")
+          .then((href) => {
+            cy.request(href).then((response) => {
+              expect(response.status).to.eql(200);
+            });
+          });
+      });
+  });
+
+  it('...Click a button containing "Bouton de test"', () => {
+    cy.get("button#main_button").should("contain", "Bouton de test").click();
   });
 });
